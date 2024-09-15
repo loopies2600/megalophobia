cc3bcee0fe11e9e028224deddde76f1594bba50b
Leandro Sotelo <frutiloopies2600@gmail.com>
2024-09-15 02:45:56 -0300

    El puto mono que camina

---
+++ scenes/entity/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody3D
+
+@export var base_speed : float = 3.0
+@export var do_gravity : bool = false
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor() && do_gravity:
+		velocity += get_gravity() * delta
+
+	var input_dir := Input.get_vector("left", "right", "forwards", "backwards")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * base_speed
+		velocity.z = direction.z * base_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, base_speed)
+		velocity.z = move_toward(velocity.z, 0, base_speed)
+
+	move_and_slide()
